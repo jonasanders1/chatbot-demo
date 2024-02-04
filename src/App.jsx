bb3400389150a3dc386c1f423d537d3774238ffb
Jonas Andersen <Jonas@anders1.com>
2024-02-04 16:58:37 +0100

    layout setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { FaArrowUp } from "react-icons/fa";
+import Logo from './assets/robot.png'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userMsg, setUserMsg] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    setIsActive(userMsg.trim() !== "");
+    console.log(isActive);
+  }, [userMsg]);
+
+  const handleUserMessage = (e) => {
+    e.preventDefault();
+    if (!userMsg.trim()) {
+      return;
+    }
+    // Add the user message to the messages state
+    setMessages([...messages, { text: userMsg, sender: "user" }]);
+    // Reset the input field after sending the message
+    setUserMsg("");
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="wrapper">
+        <div className="message-screen">
+          {messages.length === 0 ? (
+            <div className="greeting-container">
+              <img src={Logo} width={60} alt="logo"  />
+              <h1 className="greeting-txt">How can i help you today?</h1>
+            </div>
+          ) : (
+            messages.map((message, index) => (
+              <div
+                key={index}
+                className={`message-container ${
+                  message.sender === "user" ? "msg-user" : "msg-bot"
+                }`}
+              >
+                <p className="message-text">{message.text}</p>
+              </div>
+            ))
+          )}
+        </div>
+
+        <form onSubmit={handleUserMessage} className="input-container">
+          <input
+            type="text"
+            placeholder="Ask me anything..."
+            value={userMsg}
+            onChange={(e) => setUserMsg(e.target.value)}
+          />
+          <button type="submit" className={isActive ? "active" : "disabled"}>
+            <FaArrowUp size={20} color="white" />
+          </button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
