9d23de321eb28bc23cf0d81ed27fc4bab59af5e7
donadelicc <preb1anders1@gmail.com>
2024-02-11 14:31:05 +0100

    i gang

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
 // import MyComponent from "./components/MyComponent";
 
-import Chatbot from "./components/Chatbot";
+// import Chatbot from "./components/Chatbot";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="container">
       <Header/>
-      <Chatbot />
+      {/* <Chatbot /> */}
     </div>
   );
 }
